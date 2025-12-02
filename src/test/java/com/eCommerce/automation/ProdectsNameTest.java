ba1205e6653ac6a5d47820c214a8011ba8edbbc4
samira <samiradahmane29@gmail.com>
2025-12-02 02:16:18 -0500

    second commit after edit

--- src/test/java/com/eCommerce/automation/ProdectsNameTest.java
+++ src/test/java/com/eCommerce/automation/ProdectsNameTest.java
@@ -14,51 +14,52 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
 public class ProdectsNameTest extends BaseClass {
-	
+
 	WebDriverWait wait;
-	
- 
- 
-@Test 
- public void sortProductName() throws IOException {
 
-	
-ExtentTest test=extent.createTest("sorting the products A--->Z and Z--->A");
 
-	String username=prop.getProperty("username");
-	String password=prop.getProperty("password");
-	LoginPage lg=new LoginPage(driver );
-	lg.login(username,password );
-	test.info("login with valid username and valis password");
-	ProductNamePage productslist=new ProductNamePage(driver);
-    List<WebElement> products = productslist.getProductNames();
-    List<String> productNamesList = new ArrayList<>();
 
- for (WebElement pro : products) {
-     productNamesList.add(pro.getText());
- }
-System.out.println("Before Sorting:");
-      System.out.println(productNamesList);
-System.out.println("After Sorting (A–Z):");	      
-     Collections.sort(productNamesList);
-     System.out.println(productNamesList);
-   test.info("you soting order successfully A to Z");  
-System.out.println("After Sorting (Z-A):");	    
-    Collections.reverse(productNamesList);
-    System.out.println(productNamesList);
-    test.info("you soting order successfully Z to A");   
+	@Test 
+	public void sortProductName() throws IOException {
 
-	
-}	
-	
 
-	 
+		ExtentTest test=extent.createTest("sorting the products A--->Z and Z--->A");
+
+		String username=prop.getProperty("username");
+		String password=prop.getProperty("password");
+		LoginPage lg=new LoginPage(driver );
+		lg.login(username,password );
+		test.info("login with valid username and valis password");
+		ProductNamePage productslist=new ProductNamePage(driver);
+		List<WebElement> products = productslist.getProductNames();
+		List<String> productNamesList = new ArrayList<>();
+
+		
+		for (WebElement pro : products) {
+			productNamesList.add(pro.getText());
+		}
+		System.out.println("Before Sorting:");
+		System.out.println(productNamesList);
+		System.out.println("After Sorting (A–Z):");	      
+		Collections.sort(productNamesList);
+		System.out.println(productNamesList);
+		test.info("you soting order successfully A to Z");  
+		System.out.println("After Sorting (Z-A):");	    
+		Collections.reverse(productNamesList);
+		System.out.println(productNamesList);
+		test.info("you soting order successfully Z to A");   
+
+
+	}	
+
+
+
 }
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 
 
